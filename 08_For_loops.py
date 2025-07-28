e74115b8ba3f069e76db7434f3c909e44d5b6fd7
ADITYA KAUSHIK <adityakaushik150305@gmail.com>
2025-07-28 10:18:36 +0530

    loops

--- 08_For_loops.py
+++ 08_For_loops.py
@@ -4,4 +4,4 @@
 #a very simple code to print table of any number
 n = int(input("Enter a number: "))
 for i in range(1, 11):
-    print(n," X ",i,"=",5*(i)) 
+    print(n," X ",i,"=",n*(i)) 
---
+++ 09_while_loop.py
@@ -0,0 +1,11 @@
+i = 1
+while(i<6): #it checks the condition before run
+    print(i)
+    i = i + 1 
+    
+#Table using while loop
+n = int(input("Enter a number: "))
+j = 1
+while(j<11):
+    print(n," X ",j,"=",n*(j))
+    j = j + 1
